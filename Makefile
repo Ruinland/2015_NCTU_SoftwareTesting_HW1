49c8e985e02169aca9f33063de00712b6d667bf4
Ruinland <imruinland.cs00@g2.nctu.edu.tw>
2015-12-07 19:58:40 +0800

    revising message && make file

--- Makefile
+++ Makefile
@@ -1,7 +1,30 @@
-all:
-	g++ -lgtest -o test ./test.cpp triangle.cpp
-	./test
+CXX = g++
+CXXFLAGS = -std=c++14 -g -Wall -fprofile-arcs -ftest-coverage
+LDFLAGS = -lgtest
 
-.PHONY: clean
+all: unittest
+
+run: test.out
+	./test.out
+
+coverage: run
+	gcov -brac triangle.cpp
+	cat ./triangle.cpp.gcov | c++filt > ./triangle.cpp.gcov.filt
+
+## b : branch, r : relative only, a : show basic block, c : show count
+
+
+unittest: CXXFLAGS += -D__UNIT_TEST__
+unittest: test.out
+
+test.out: test.cpp triangle.cpp
+	$(CXX) -o test.out $(CXXFLAGS) $(LDFLAGS) ./test.cpp ./triangle.cpp
+
 clean:
-	rm -rf ./test
+	rm -f *.out
+	rm -f *.gcno
+	rm -f *.gcov
+	rm -f *.gcda
+	rm -f *.filt
+
+.PHONY: clean all
--- test.cpp
+++ test.cpp
@@ -13,29 +13,70 @@
 //#define NUM_NORM 5
 //#define NUM_ROBU 6
 
-int main(int argc, char* argv[]) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-	}
-TEST(NormalTestSingle,TriangleTest) {
-	EXPECT_EQ(Equilateral, judge_triangle(1,1,1));
-	EXPECT_EQ(Equilateral, judge_triangle(2,2,2));
-	EXPECT_EQ(Equilateral, judge_triangle(100,100,100));
-	EXPECT_EQ(Equilateral, judge_triangle(199,199,199));
-	
-	EXPECT_EQ(Isosceles, judge_triangle(100,100,199));
-	}
+int main(int argc, char *argv[]){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
 
-TEST(RobustTestSingle,TriangleTest) {
-	EXPECT_EQ(INVALID, judge_triangle(0,0,0));
+/* Testing Code */
+// Weak Robust test for check_triangle_type()
+// SideLength1to199
+// SideLength0or200
+// SideLengthTriInequalityNormal
+// SideLengthTriInequalityRobust
 
-	EXPECT_EQ(Equilateral, judge_triangle(1,1,1));
-	EXPECT_EQ(Equilateral, judge_triangle(2,2,2));
-	EXPECT_EQ(Equilateral, judge_triangle(100,100,100));
-	EXPECT_EQ(Equilateral, judge_triangle(199,199,199));
-	EXPECT_EQ(INVALID, judge_triangle(200,200,200));
-	
-	EXPECT_EQ(Isosceles, judge_triangle(100,100,199));
-	EXPECT_EQ(NotTriangle, judge_triangle(99,99,198));
-	EXPECT_EQ(INVALID, judge_triangle(100,100,200)); //This is due to spec
-	}
+// triangle boundary value of 0 ~ 200: {0, 1, 2, 110, 198, 199, 200}
+TEST(CheckTriangleTypeTest, SideLength1to199){
+    // all normal
+    EXPECT_EQ(Equilateral, judge_triangle(110, 110, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(150, 150, 110));
+    EXPECT_EQ(Scalene, judge_triangle(110, 113, 116));
+
+    // one boundary and others normal
+    EXPECT_EQ(Isosceles, judge_triangle(1, 110, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(2, 110, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(198, 110, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(199, 110, 110));
+
+    EXPECT_EQ(Isosceles, judge_triangle(110, 1, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 2, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 198, 110));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 199, 110));
+
+    EXPECT_EQ(Isosceles, judge_triangle(110, 110, 1));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 110, 2));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 110, 198));
+    EXPECT_EQ(Isosceles, judge_triangle(110, 110, 199));
+}
+
+TEST(CheckTriangleTypeTest, SideLength0or200){
+    EXPECT_EQ(INVALID, judge_triangle(0, 110, 110));
+    EXPECT_EQ(INVALID, judge_triangle(200, 110, 110));
+
+    EXPECT_EQ(INVALID, judge_triangle(110, 0, 110));
+    EXPECT_EQ(INVALID, judge_triangle(110, 200, 110));
+
+    EXPECT_EQ(INVALID, judge_triangle(110, 110, 0));
+    EXPECT_EQ(INVALID, judge_triangle(110, 110, 200));
+}
+
+// triangle boundary value of a < b+c, b < a+c, c < a+b: { a = b+c, a = b+c-1, a = b+c-2, a < b+c }
+TEST(CheckTriangleTypeTest, SideLengthTriInequalityNormal){
+    EXPECT_EQ(Scalene, judge_triangle(100, 75, 50));
+    EXPECT_EQ(Scalene, judge_triangle(100, 52, 50));
+    EXPECT_EQ(Scalene, judge_triangle(100, 51, 50));
+
+    EXPECT_EQ(Scalene, judge_triangle(75, 100, 50));
+    EXPECT_EQ(Scalene, judge_triangle(52, 100, 50));
+    EXPECT_EQ(Scalene, judge_triangle(51, 100, 50));
+
+    EXPECT_EQ(Scalene, judge_triangle(75, 50, 100));
+    EXPECT_EQ(Scalene, judge_triangle(52, 50, 100));
+    EXPECT_EQ(Scalene, judge_triangle(51, 50, 100));
+}
+
+TEST(CheckTriangleTypeTest, SideLengthTriInequalityRobust){
+    EXPECT_EQ(NotTriangle, judge_triangle(100, 50, 50));
+    EXPECT_EQ(NotTriangle, judge_triangle(50, 100, 50));
+    EXPECT_EQ(NotTriangle, judge_triangle(50, 50, 100));
+}
